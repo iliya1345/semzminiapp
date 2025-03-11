f4f3fa20143f570928c61cbe4148db2e8daa0d03
mahdi bahrami <mahdibahrami@mahdis-MacBook-Pro.local>
2025-03-11 13:41:54 +0300

    m

--- utils/firebaseUtils.ts
+++ utils/firebaseUtils.ts
@@ -58,7 +58,7 @@ export async function getPurchedSkins(
     return data;
   } catch (error) {
     console.error('Error fetching document:', error);
-    throw new Error(`Unable to fetch document: ${error}`);
+    return {};
   }
 }
 
